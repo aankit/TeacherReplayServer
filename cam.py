2385842b04d33d068ce7c6dc86a371eff9f2daa0
aankit <aankit.patel@gmail.com>
2015-03-21 22:46:00 -0400

    reliable picam

--- cam.py
+++ cam.py
@@ -18,45 +18,43 @@ import subprocess
 
 @app.route('/record/<state>')
 def record(state):
-    return_text = ""
     if state == 'on':
-        cam = subprocess.call(["sudo", "service", "picam", "start"])
-        if cam == 0:
-            return_text += "cam on:"
-            record = subprocess.call(["touch", "/home/pi/hooks/start_record"])
-            if record == 0:
-                return return_text + "we are now recording video and audio!"
+        try:
+            subprocess.call("/var/www/replay/shell_scripts/cam_on.sh")
+        except:
+            return "we are not recording"
+        return "its on"
     elif state == 'off':
-        record = subprocess.call(["touch", "/home/pi/hooks/stop_record"])
-        if record == 0:
-            cam = subprocess.call(["sudo", "service", "picam", "stop"])
-            if cam == 0:
-                return "successfully shutdown"
+        try:
+            subprocess.call("/var/www/replay/shell_scripts/cam_off.sh")
+        except:
+            return "we are stiiiil recording"
+        return "its off"
     else:
         return "Please either send on or off. Thanks, your friendly neighborhood API."
 
-@app.route('/cam/<state>')
-def cam(state):
-    if state == 'on':
-        cmd = ["sudo", "service", "picam", "start"]
-        p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
-        out, err = p.communicate()
-        return out
-    elif state == 'off':
-        cmd = ["sudo", "service", "picam", "stop"]
-        p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
-        out, err = p.communicate()
-        return out
-    else:
-        return "Please either send on or off. Thanks, your friendly neighborhood API."
+#@app.route('/cam/<state>')
+#def cam(state):
+#    if state == 'on':
+#        cmd = ["sudo", "service", "picam", "start"]
+#        p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
+#        out, err = p.communicate()
+#        return out
+#    elif state == 'off':
+#        cmd = ["sudo", "service", "picam", "stop"]
+#        p = subprocess.Popen(cmd, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
+#        out, err = p.communicate()
+#        return out
+#    else:
+#        return "Please either send on or off. Thanks, your friendly neighborhood API."
 
 #@app.route('/schedule', methods=['GET', 'POST'])
 #def schedule():
 #    return 'schedule page'
 
-@app.route('/marker/<timestamp>/<mode>')
-def marker(timestamp, mode):
-    #mode_obj = Mode(mode)
-    return '%s with %s mode' %(timestamp, mode)
+#@app.route('/marker/<timestamp>/<mode>')
+#def marker(timestamp, mode):
+#    #mode_obj = Mode(mode)
+#    return '%s with %s mode' %(timestamp, mode)
 
 
